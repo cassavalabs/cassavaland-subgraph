96b36ffda64bc806913c696fecfa9d381a1ebc90
Emmanuel <opensoftweb@gmail.com>
2022-07-25 06:57:58 +0100

    Initialize repository

---
+++ src/utils/index.ts
@@ -0,0 +1,7 @@
+export * from "./fetch/account";
+export * from "./fetch/allNfts";
+export * from "./fetch/erc1155";
+export * from "./fetch/erc721";
+export * from "./constants";
+export * from "./interface";
+export * from "./uri";
